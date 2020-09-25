3d0f6adce32c85c8616b173377c7027720ac73e8
CarlosMorette <carlos.morette@outlook.com>
2020-09-25 15:33:28 -0300

    Small fix

--- front-web/src/components/RoutePermission/RoutePermission.jsx
+++ front-web/src/components/RoutePermission/RoutePermission.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { 
     verifyAuthenticatedUser as verify, 
     decryptPayloadJwtAndReturnObject as decrypt, 
@@ -12,11 +12,16 @@ export default function RoutePermission({ path, role, component: Component}){
     const [authenticated] = useState(verify())
     const [roleJwt] = useState(decrypt(getInLocalStorage(KEY_USER_JWT)).Role)
 
+    useEffect(() => {
+        console.log(authenticated)
+        console.log(roleJwt)
+    },[])
+
     return (
         <Route
             path={path}
-            render={props => authenticated && roleJwt === role ? <Component {...props}/> :
-            role === 0 ? <Component {...props} /> : <Redirect to={{ pathname: "/" }} />}
+            render={props => authenticated ? (roleJwt === role) ? <Component {...props}/> : <Redirect to={{ pathname: "/" }}/> 
+            : role === 0 ? <Component {...props}/> : <Redirect to={{ pathname: "/" }}/>}
         />
     )
 }
